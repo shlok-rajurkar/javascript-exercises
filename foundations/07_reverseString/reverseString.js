9b614753dbccbc2b6c71ffce7b65170b4c4fc4c5
Dragonappendix <228423547+Dragonappendix@users.noreply.github.com>
2026-02-17 17:36:26 -0800

    Complete 07

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    result = "";
+    for (let i = string.length - 1; i > -1; i--) {
+        result += string[i];
+    }
+    return result;
 };
 
 // Do not edit below this line
