4f452fcdfdb2a8e4ffb89c868dae6925962f3da8
Dragonappendix <228423547+Dragonappendix@users.noreply.github.com>
2026-02-17 18:14:01 -0800

    Complete 09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(start, stop) {
+    if (
+        typeof start !== 'number' |
+        typeof stop !== 'number' |
+        Math.floor(start) !== start |
+        Math.floor(stop) !== stop |
+        start < 0 |
+        stop < 0 
+    ) {
+        return "ERROR";
+    }
+    let result = 0;
+    min = Math.min(start, stop);
+    max = Math.max(start, stop);
+    for (let i = min; i <= max; i++) {
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
