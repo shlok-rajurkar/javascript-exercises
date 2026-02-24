b5b49ddec814333006168c9f08d9d506f2c7a20e
Dragonappendix <228423547+Dragonappendix@users.noreply.github.com>
2026-02-23 16:02:41 -0800

    Complete 12

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(inputArray) {
+	return inputArray.reduce((sum, item) => {
+    return sum + item;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((product, num) => {
+    return product * num;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let result = 1;
+  for (let i = 0; i < b; i++) {
+    result *= a;
+  }
+  return result;	
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = 1;
+  for (let i = num; i > 0; i--) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
