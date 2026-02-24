ebf89fc98142746ffc0eb31e11afc0bc22045eba
Dragonappendix <228423547+Dragonappendix@users.noreply.github.com>
2026-02-23 16:24:32 -0800

    Complete 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    const wordArray = [...word].filter((char) => {
+        return !([".", " ", ",", "!"].includes(char));
+    }).map((char) => {
+        return char.toLowerCase();
+    });
+    const revWordArray = wordArray.toReversed();
+    console.log(wordArray);
+    console.log(revWordArray);
+    for (let i = 0; i < wordArray.length; i++) {
+        if (wordArray[i] !== revWordArray[i]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
