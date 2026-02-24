bca02aad9cbdd51324563243b84f2f51e47f4d58
Dragonappendix <228423547+Dragonappendix@users.noreply.github.com>
2026-02-23 16:28:05 -0800

    Fix merge conflict

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,11 +1,7 @@
 const leapYears = function(year) {
     if (year % 4 === 0) {
         if (year % 100 === 0) {
-            if (year % 400 === 0) {
-                return true;
-            } else {
-                return false;
-            }
+            return (year % 400 === 0);
         } else {
             return true;
         }
