131e7d2689e2fad1c4ce5e275e897efcdcbb8552
Dragonappendix <228423547+Dragonappendix@users.noreply.github.com>
2026-02-23 17:12:15 -0800

    Complete 16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(personArray) {
+    function calcAge(person) {
+        if (person.yearOfDeath) {
+            return person.yearOfDeath - person.yearOfBirth;
+        } else {
+            return 2026 - person.yearOfBirth;
+        }
+    }
+    return personArray.reduce((oldestPerson, currentPerson) => {
+        if (calcAge(oldestPerson) < calcAge(currentPerson)) {
+            return currentPerson;
+        } else {
+            return oldestPerson;
+        }
+    }, personArray[0]);
 };
 
 // Do not edit below this line
